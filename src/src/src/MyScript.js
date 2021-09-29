60321e2008c14ec18ff11766aaaacaa2eb021ade
rodionmirskij <85698580+rodionmirskij@users.noreply.github.com>
2021-09-29 20:28:22 +0300

    Add files via upload

---
+++ src/src/src/MyScript.js
@@ -0,0 +1,49 @@
+"use strict"
+
+let kindOf = prompt("Выберите цифру. Тип сайта: 1-визитка, 2-портал, 3-корпоративный, 4-друое");
+console.log(kindOf);
+
+if (kindOf = "1") {
+    console.log("5000");
+    }
+
+    else if (kindOf = "2");{
+        console.log("7000");
+    }
+        
+    else if (kindOf = "3"){
+        console.log("4000");
+        }
+    
+    else {
+        console.log("2000");
+    }
+
+
+
+let disign = prompt("Выберите цифру. Дизайн: 1-авторский, 2-индивидуальный");
+console.log(disign);
+
+if (kindOf = "1") {
+    console.log("2000");
+    }
+    else {
+        console.log("5000");
+    }
+
+
+
+let flexibility = prompt("Выберите цифру. Адаптивность: 1-адаптивный 2-не адаптивный");
+console.log(flexibility);
+
+if (kindOf = "1") {
+    console.log("2000");
+    }
+    else {
+        console.log("5000");
+    }
+
+
+let sum = "";
+alert(sum);
+
