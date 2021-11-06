4bf0630b428d0ae552b8854641a99a1ed9c615f2
rodionmirskij <85698580+rodionmirskij@users.noreply.github.com>
2021-11-06 17:18:22 +0300

    Add files via upload

--- src/src/src/php/mail2.php
+++ src/src/src/php/mail2.php
@@ -11,18 +11,9 @@ $mess = $_POST['messenger'];
 
 
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'marskiy.sites@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'dvgdnzdbnzvltrzmnk18'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('marskiy.sites@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('rodion.mirskig@mail.ru');     // Кому будет уходить письмо 
+$mail->setFrom('rodion.mirskij@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('marskiy.sites@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
